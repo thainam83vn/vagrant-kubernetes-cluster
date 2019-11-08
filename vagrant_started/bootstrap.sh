2b5bb0177134f26ccc9178e48603c58e9a2575f1
thai <thai@master-node.fios-router.home>
2019-11-08 18:46:47 -0500

    add vagrant

--- vagrant_started/bootstrap.sh
+++ vagrant_started/bootstrap.sh
@@ -1,13 +1,77 @@
-apt-get update -y
-apt-get install docker.io -y
-systemctl enable docker
-apt-get install curl -y
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get install kubeadm -y
-kubeadm reset -f
-echo "Joining Kubernete"
-hostname
-swapoff -a
-kubeadm join 192.168.2.227:6443 --token neslbs.yars2oqeedtzsy1w --discovery-token-ca-cert-hash sha256:c8f42c63ffe6597d0b71678f1b89a7ff70ccf83ecd615ba7e5b4e5b498fce518 
+#!/bin/bash
 
+# Update hosts file
+echo "[TASK 1] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.42.42.100 kmaster.example.com kmaster
+172.42.42.101 kworker1.example.com kworker1
+172.42.42.102 kworker2.example.com kworker2
+EOF
+
+# Install docker from Docker-ce repository
+echo "[TASK 2] Install docker container engine"
+yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
+yum install -y -q docker-ce >/dev/null 2>&1
+
+# Enable docker service
+echo "[TASK 3] Enable and start docker service"
+systemctl enable docker >/dev/null 2>&1
+systemctl start docker
+
+# Disable SELinux
+echo "[TASK 4] Disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+# Stop and disable firewalld
+echo "[TASK 5] Stop and Disable firewalld"
+systemctl disable firewalld >/dev/null 2>&1
+systemctl stop firewalld
+
+# Add sysctl settings
+echo "[TASK 6] Add sysctl settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+# Disable swap
+echo "[TASK 7] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+# Add yum repo file for Kubernetes
+echo "[TASK 8] Add yum repo file for kubernetes"
+cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Install Kubernetes
+echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
+yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
+
+# Start and Enable kubelet service
+echo "[TASK 10] Enable and start kubelet service"
+systemctl enable kubelet >/dev/null 2>&1
+systemctl start kubelet >/dev/null 2>&1
+
+# Enable ssh password authentication
+echo "[TASK 11] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "[TASK 12] Set root password"
+echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
+
+# Update vagrant user's bashrc file
+echo "export TERM=xterm" >> /etc/bashrc
